1a4e08bece0455617a3aa8b0e0dd28c31875e95b
koala1996 <61231082+koala1996@users.noreply.github.com>
2020-02-20 17:19:06 +0100

    Deploy

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -4,6 +4,6 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
-    msg = {'ec2':json.dumps(response), 'message':json.dumps('hello peter !!!')}
+    msg = {'ec2':json.dumps(response), 'message':json.dumps('hello peter !!!!!')}
 
     return {"statusCode": 200, "body": json.dumps(msg)}
